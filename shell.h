6bea9ee25e933801b9c3a348f02a5058a9b082d4
Michael Mwangi <michaelmichire2016@gmail.com>
2023-11-02 00:02:55 -0700

    The main header file for this project

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#endif
