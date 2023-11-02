a79003bac44031e045eadcf0b8c11c17fd2b9f70
Michael Mwangi <michaelmichire2016@gmail.com>
2023-11-02 01:56:33 -0700

    The main header

--- shell.h
+++ shell.h
@@ -18,4 +18,49 @@ char *_memset(char *s, char b, unsigned int n);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void ffree(char **str);
 
+/**
+ * struct pseudo_arg - contains arguments to pass to fns
+ * @argv:an array of strings
+ * @argc: the argument count
+ * @number_error: the error code for exit
+ * @file_name: the filename
+ * @environ: custom modified copy of environ
+ * @alias: the alias node
+ * @process: the return status of the command executed
+ * @command_buffer_type: The buffer type
+ * @history_count: the history line number count
+ * @read_file_descriptor: the fd to read line input
+ * @command_buffer: address of pointer to command_buffer
+ * @environ_c: shows if environ was changed
+ * @history: the history node
+ * @env: linked list copy of environ
+ * @lcf: linecount flag
+ * @lc: the error count
+ * @path: a string path
+ * @arg: a string generated from getline
+ */
+
+typedef struct pseudo_arg
+{
+	char **argv;
+	int argc;
+	int number_error;
+	char *file_name;
+	char **environ;
+	list_t *alias;
+	int process;
+	int command_buffer_type;
+	int history_count;
+	int read_file_descriptor;
+	char **command_buffer;
+	int environ_c;
+	list_t *history;
+	list_t *env;
+	int lcf;
+	unsigned int lc;
+	char *path;
+	char *arg;
+} shell_t;
+
+
 #endif
