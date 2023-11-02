8ec6df8ebc2010dc21101466ce3c1ca98f539ee1
Michael Mwangi <michaelmichire2016@gmail.com>
2023-11-02 04:47:00 -0700

    The main header file for this project

--- shell.h
+++ shell.h
@@ -118,4 +118,10 @@ int chain_check(shell_t *cont, char *buf, size_t *ptr);
 void handle_chain(shell_t *cont, char *buf, size_t *ptr, size_t a, size_t len);
 int var_substitute(shell_t *cont);
 
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+void _puts(char *str);
+char *_strcpy(char *dest, char *src);
+char *_strdup(const char *str);
+
 #endif
